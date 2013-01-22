362548c24dfa6c2c5c8df49e66db05ea70353638
mmontuori <michael.montuori@gmail.com>
2013-01-22 15:51:40 -0500

    MROM work in progress

--- config/themes_common.mk
+++ config/themes_common.mk
@@ -7,3 +7,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
        vendor/cm/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
+
+PRODUCT_PROPERTY_OVERRIDES += \
+       persist.sys.themeId = Mixer \
+       persist.sys.themePackageName = com.mrdsl.theme.mixerics
